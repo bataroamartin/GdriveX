e958e3f07f7e98204b1ac602e81c5e0e787b4431
gdriveadarsh <91414707+gdriveadarsh@users.noreply.github.com>
2021-09-26 19:44:50 +0530

    Update help.py

--- bot/plugins/help.py
+++ bot/plugins/help.py
@@ -3,7 +3,7 @@ from pyrogram import Client, filters
 from bot.config import Messages as tr
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-@Client.on_message(filters.command(["ytdl"]))
+@Client.on_message(filters.command(['ytdl']))
 async def help_user(bot, update):
     # logger.info(update)
     await client.send_message(
